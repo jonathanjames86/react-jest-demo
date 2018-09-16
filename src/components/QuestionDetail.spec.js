3f41c1195aa9a345a105036b4d36c5764f333750
Jonathan Putnam <jonathan.putnam86@gmail.com>
2018-09-16 11:22:42 -0600

    Setup basic jest environment and mocking

---
+++ src/components/QuestionDetail.spec.js
@@ -0,0 +1,11 @@
+describe('The Question Detail Component', ()=> {
+    afterAll(() => {
+        console.log('Tests finished in QuestionDetail.spec');
+    })
+    it('should display a list of items', () => {
+        expect(3 + 2).toEqual(5);
+    })
+    it('should display a list of things', () => {
+        expect(3 + 2).toEqual(5);
+    })
+});
